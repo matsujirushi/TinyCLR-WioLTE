e117647db4344e1a186667898329df264fd84332
matsujirushi <matsujirushi@live.jp>
2017-12-30 19:56:06 +0900

    DigitalWriteを追加

---
+++ Projects/DigitalWrite/Program.cs
@@ -0,0 +1,21 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using System.Threading;
+
+namespace DigitalWrite
+{
+    class Program
+    {
+        static void Main()
+        {
+            GpioPin pin = GpioController.GetDefault().OpenPin(38);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            while (true)
+            {
+                pin.Write(GpioPinValue.High);
+                Thread.Sleep(1);
+                pin.Write(GpioPinValue.Low);
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
