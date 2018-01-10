0e3a683e087de93cf78e90b7c3b8c7f5544e1187
matsujirushi <matsujirushi@live.jp>
2018-01-10 21:59:17 +0900

    LTEモジュールと通信（受信）

--- Projects/SampleApp/Program.cs
+++ Projects/SampleApp/Program.cs
@@ -1,6 +1,11 @@
-﻿using Seeed.TinyCLR.WioLTE;
+﻿using GHIElectronics.TinyCLR.Devices.SerialCommunication;
+using Seeed.TinyCLR.WioLTE;
 using System.Diagnostics;
 using System.Threading;
+using GHIElectronics.TinyCLR.Pins;
+using GHIElectronics.TinyCLR.Storage.Streams;
+using System;
+using System.Text;
 
 namespace SampleApp
 {
@@ -24,6 +29,24 @@ namespace SampleApp
 
             Debug.WriteLine("### Turn on or reset.");
             Wio.TurnOnOrReset();
+
+            SerialDevice ser = SerialDevice.FromId(STM32F4.UartPort.Usart2);
+            ser.BaudRate = 115200;
+            ser.ReadTimeout = TimeSpan.Zero;
+            var serReader = new DataReader(ser.InputStream);
+            var serWriter = new DataWriter(ser.OutputStream);
+
+            while (true)
+            {
+                var i = serReader.Load(1);
+                if (i > 0)
+                {
+                    byte b = serReader.ReadByte();
+                    Debug.WriteLine("Recieved: " + b + ":" + new string(Encoding.UTF8.GetChars(new byte[] { b })));
+                }
+
+                Thread.Sleep(10);
+            }
         }
     }
 }
